95bf95c6882c063457e0269b0efc765b470827fd
Gustav Sörnäs <gustav@sornas.net>
2020-10-11 23:29:26 +0200

    create command.rs

---
+++ mumd/src/command.rs
@@ -0,0 +1,14 @@
+enum Command {
+    ChannelJoin {
+        channel_id: u32,
+    },
+    ChannelList,
+    ServerConnect {
+        host: String,
+        port: u16,
+        username: String,
+        accept_invalid_cert: bool, //TODO ask when connecting
+    },
+    ServerDisconnect,
+    Status,
+}
