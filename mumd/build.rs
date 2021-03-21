abbea6a4a20ed1562d99ce5942154352bc545eb5
Gustav Sörnäs <gustav@sornas.net>
2021-03-21 14:59:03 +0100

    commit_hash returns option instead

--- mumd/build.rs
+++ mumd/build.rs
@@ -5,21 +5,17 @@ fn main() {
         "{} {}-{}",
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION"),
-        commit_hash(),
+        commit_hash().unwrap_or_else(|| "???".to_string()),
     );
 
     println!("cargo:rustc-env=VERSION={}", version);
 }
 
-fn commit_hash() -> String {
+fn commit_hash() -> Option<String> {
     let output = Command::new("git")
                          .arg("show")
                          .arg("--pretty=format:%h")  // abbrev hash
                          .current_dir(env!("CARGO_MANIFEST_DIR"))
                          .output();
-    if let Ok(output) = output {
-        String::from_utf8_lossy(&output.stdout).to_string()
-    } else {
-        String::from("???")
-    }
+    output.ok().map(|o| String::from_utf8_lossy(&o.stdout).to_string())
 }
